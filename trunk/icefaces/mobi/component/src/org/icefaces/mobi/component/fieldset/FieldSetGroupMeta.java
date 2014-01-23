c94b982dded153eae76f0571128a681664d0b26d
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-01-23 17:06:11 +0000

    MOBI-1000 added mobi:fieldsetRow and mobi:fieldsetGroup components
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@39745 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/mobi/component/src/org/icefaces/mobi/component/fieldset/FieldSetGroupMeta.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobi.component.fieldset;
+
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+import org.icefaces.mobi.util.TLDConstants;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+
+@Component(
+        tagName = "fieldsetGroup",
+        componentClass = "org.icefaces.mobi.component.fieldset.FieldSetGroup",
+        rendererClass = "org.icefaces.mobi.component.fieldset.FieldSetGroupRenderer",
+        generatedClass = "org.icefaces.mobi.component.fieldset.FieldSetGroupBase",
+        componentType = "org.icefaces.FieldSetGroup",
+        rendererType = "org.icefaces.FieldSetGroupRenderer",
+        extendsClass = "javax.faces.component.UIComponentBase",
+        componentFamily = "org.icefaces.FieldSetGroup",
+        tlddoc = "fieldSetGroup organizes a grouping of unordered list items. The immediate children " +
+                "must be fieldsetRow components. "
+)
+
+@ResourceDependencies({
+		@ResourceDependency(library = "org.icefaces.component.fieldset", name = "fieldset.css"),
+        @ResourceDependency(library = "org.icefaces.component.util", name = "component.js")
+})
+public class FieldSetGroupMeta extends UIComponentBaseMeta {
+	
+    @Property(defaultValue="true",
+    		  tlddoc = "When \"true\", renders padding around list group. ")
+    private boolean inset;
+ 
+    @Property(tlddoc = TLDConstants.STYLE)
+    private String style;
+
+    @Property(tlddoc = TLDConstants.STYLECLASS)
+    private String styleClass;
+
+}
---
+++ trunk/icefaces/mobi/component/src/org/icefaces/mobi/component/fieldset/FieldSetRowMeta.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobi.component.fieldset;
+
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+import org.icefaces.mobi.util.TLDConstants;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+
+@Component(
+        tagName = "fieldsetRow",
+        componentClass = "org.icefaces.mobi.component.fieldset.FieldSetRow",
+        rendererClass = "org.icefaces.mobi.component.fieldset.FieldSetRowRenderer",
+        generatedClass = "org.icefaces.mobi.component.fieldset.FieldSetRowBase",
+        componentType = "org.icefaces.FieldSetRow",
+        rendererType = "org.icefaces.FieldSetRowRenderer",
+        extendsClass = "javax.faces.component.UIComponentBase",
+        componentFamily = "org.icefaces.FieldSetRow",
+        tlddoc = "fieldsetRow is used within a fieldsetGroup tag to group unordered items."
+)
+
+
+@ResourceDependencies({
+		@ResourceDependency(library = "org.icefaces.component.fieldset", name = "fieldset.css"),
+        @ResourceDependency(library = "org.icefaces.component.util", name = "component.js")
+})
+public class FieldSetRowMeta extends UIComponentBaseMeta {
+        	
+
+    @Property(defaultValue="false",
+    		  tlddoc = "When \"true\", will render the item with a list header style. ")
+    private boolean group;
+
+    @Property(tlddoc = TLDConstants.STYLE)
+    private String style;
+
+    @Property(tlddoc = TLDConstants.STYLECLASS)
+    private String styleClass;
+
+}
