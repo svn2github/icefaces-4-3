bd6022f9e04e68aad4f3bf0655736b5a51b9ec04
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-06-12 18:59:45 +0000

    ICE-10077 added new line character so that the call to download the file via the resource url doesn't appear commented out
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@41328 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/ace/component/src/org/icefaces/ace/component/dataexporter/DataExporterRenderer.java
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/component/dataexporter/DataExporterRenderer.java
@@ -123,7 +123,7 @@ public class DataExporterRenderer extends CoreRenderer {
 		// load file
 		String path = exporter.getPath(clientId);
 		if (path != null) {
-			writer.write("ice.ace.DataExporters['" + clientId + "'].url('" + path + "');");
+			writer.write("\nice.ace.DataExporters['" + clientId + "'].url('" + path + "');");
 		}
 		
 		writer.endElement("script");
