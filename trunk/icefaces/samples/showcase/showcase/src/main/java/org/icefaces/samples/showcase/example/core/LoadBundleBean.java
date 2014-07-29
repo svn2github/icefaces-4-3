fc55676ba0b25230ab8834264b0a95f0c3767585
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-07-29 19:47:59 +0000

    ICE-10082 Added icecore:loadBundle demo.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@42027 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/core/LoadBundleBean.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.core;
+
+import org.icefaces.samples.showcase.metadata.annotation.ComponentExample;
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResource;
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResources;
+import org.icefaces.samples.showcase.metadata.annotation.ResourceType;
+import org.icefaces.samples.showcase.metadata.context.ComponentExampleImpl;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.io.Serializable;
+import java.util.Locale;
+
+@ComponentExample(
+        title = "DefaultAction Component",
+        description = "The <b>&lt;icecore:loadBundle&gt;</b> component allows the loading of a resource bundle to be used for locale specific messages. The defined bundle is completely dynamic, meaning that if any of its attribute change at any time there will be no need to reload the page, session or application.",
+        example = "/resources/examples/core/load-bundle.xhtml"
+)
+
+@ExampleResources(
+        resources = {
+                @ExampleResource(type = ResourceType.xhtml,
+                        title="loadBundle.xhtml",
+                        resource = "/resources/examples/core/load-bundle.xhtml"),
+                @ExampleResource(type = ResourceType.java,
+                        title="LoadBundleBean.java",
+                        resource = "/WEB-INF/classes/org/icefaces/samples/showcase/example/core/LoadBundleBean.java"),
+        }
+)
+
+@ManagedBean
+@ViewScoped
+public class LoadBundleBean extends ComponentExampleImpl<LoadBundleBean> implements Serializable {
+    private String language = "en";
+
+    public LoadBundleBean() {
+        super(LoadBundleBean.class);
+    }
+
+    @PostConstruct
+    public void initMetaData() {
+        super.initMetaData();
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+
+        UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+        if ("en".equals(language)) {
+            viewRoot.setLocale(Locale.ENGLISH);
+        } else if ("fr".equals(language)) {
+            viewRoot.setLocale(Locale.FRENCH);
+        } else if ("it".equals(language)) {
+            viewRoot.setLocale(Locale.ITALIAN);
+        }
+    }
+}
---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/core/LoadBundleBeanResources.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.core;
+
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResource;
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResources;
+import org.icefaces.samples.showcase.metadata.annotation.ResourceType;
+import org.icefaces.samples.showcase.metadata.context.ComponentExampleImpl;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+@ExampleResources(
+        resources = {
+                @ExampleResource(type = ResourceType.tld,
+                        title = "icecore:loadBundle",
+                        resource = "http://res.icesoft.org/docs/v4_latest/core/comps/tld/icecore/loadBundle.html"),
+                @ExampleResource(type = ResourceType.wiki,
+                        title = "icecore:loadBundle",
+                        resource = "http://www.icesoft.org/wiki/display/ICE/loadBundle")
+        }
+)
+@ManagedBean
+@ApplicationScoped
+public class LoadBundleBeanResources extends ComponentExampleImpl<LoadBundleBeanResources> implements Serializable {
+    public LoadBundleBeanResources() {
+        super(LoadBundleBeanResources.class);
+    }
+
+    @PostConstruct
+    public void initMetaData() {
+        super.initMetaData();
+    }
+}
