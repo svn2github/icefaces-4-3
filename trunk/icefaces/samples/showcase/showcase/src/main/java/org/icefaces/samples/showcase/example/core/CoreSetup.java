bcd6ed2fe702d95d3a796ea6a7852adc1e4159e0
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-07-31 19:33:59 +0000

    ICE-10082 Introduced CoreSetup bean that configures the NavigationModel so that demo's content is always shown. Remove unused namespaces in *.xhtml files.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@42052 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/core/CoreSetup.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.core;
+
+import org.icefaces.samples.showcase.view.navigation.NavigationModel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CoreSetup {
+    @ManagedProperty(value = "#{navigationModel}")
+    private NavigationModel model;
+    private boolean invoked = false;
+
+    public CoreSetup() {
+    }
+
+    public NavigationModel getModel() {
+        return model;
+    }
+
+    public void setModel(NavigationModel model) {
+        this.model = model;
+        //need to set "renderAsExample" property to have the demo's content shown
+        model.setRenderAsExample(true);
+    }
+
+    public boolean isInvoked() {
+        return invoked;
+    }
+}
