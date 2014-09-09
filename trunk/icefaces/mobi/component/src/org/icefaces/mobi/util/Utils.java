19dd77085cb9f1275c7849aecbd6f3ecb4090ae0
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-09-09 19:06:17 +0000

    ICE-10175 fix to flush and close OutputStream for camera and camcorder uploads, in order to avoid file locks when disposing of uploaded files
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@42534 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/mobi/component/src/org/icefaces/mobi/util/Utils.java
+++ trunk/icefaces/mobi/component/src/org/icefaces/mobi/util/Utils.java
@@ -385,7 +385,8 @@ public class Utils {
                 }
             }
         }
-        
+		out.flush();
+		out.close();
     }
 
     public static int copyStream(InputStream in, OutputStream out, int start,
@@ -412,6 +413,8 @@ public class Utils {
                 count += l;
             }
         }
+		out.flush();
+		out.close();
         return count;
     }
 
