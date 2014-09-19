5e9a44f1583e19ebea5b71a6e308fa2d9462cd75
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-09-19 00:19:45 +0000

    Load page with the saved URL to update the page with the right content.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@42588 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/samples/showcase/showcase/src/main/webapp/resources/javascript/icefaces_showcase.js
+++ trunk/icefaces/samples/showcase/showcase/src/main/webapp/resources/javascript/icefaces_showcase.js
@@ -1,16 +1,18 @@
-var backButton = function() {
+function restoreURL(e) {
+    if (e.state) {
+        location.href = e.state.src;
+    }
+}
+
+function backButton() {
     if( window.history && window.history.pushState ){
         if (window.addEventListener) {
-            window.addEventListener("popstate", function(e) {
-                    location.reload();
-            });
+            window.addEventListener("popstate", restoreURL);
         } else {
-            window.attachEvent("popstate", function(e) {
-                    location.reload();
-            });
+            window.attachEvent("popstate", restoreURL);
         }
     }
-};
+}
 
 if (window.addEventListener) {
     window.addEventListener('load', backButton, true);
@@ -21,6 +23,6 @@ if (window.addEventListener) {
 function updateAddressBarURL(grp, exp){
     var hypertextReference = '//' + location.host + location.pathname + '?grp=' + grp + '&exp=' + exp;
     if( window.history && window.history.pushState ){ //check if browser supports HTML5 history pushState
-        history.pushState({ src: hypertextReference }, null, hypertextReference); //set the current url, pass in a null state object
+        history.pushState({ src: location.href }, null, hypertextReference); //set the current url, pass in a null state object
     }
 }
