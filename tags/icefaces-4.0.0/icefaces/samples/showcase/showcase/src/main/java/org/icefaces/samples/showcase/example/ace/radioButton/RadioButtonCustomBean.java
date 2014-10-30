83fbe680ce0089757107a8c727d69ffdabf116ec
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-10-30 00:56:21 +0000

    Update copyright header.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@43284 8668f098-c06c-11db-ba21-f49e70c34f74

--- tags/icefaces-4.0.0/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/radioButton/RadioButtonCustomBean.java
+++ tags/icefaces-4.0.0/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/radioButton/RadioButtonCustomBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the
@@ -23,26 +23,6 @@ import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 
-/**
- * Copyright 2010-2013 ICEsoft Technologies Canada Corp.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * <p/>
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p/>
- * User: Nils Lundquist
- * Date: 2013-02-11
- * Time: 12:31 PM
- */
 @ComponentExample(
         title = "example.ace.radioButton.custom.title",
         description = "example.ace.radioButton.custom.description",
