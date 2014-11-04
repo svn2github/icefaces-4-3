415644257459f6991a4fe22fc25521b1900fbf5d
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-11-04 23:52:20 +0000

    ICE-9484 - Update path for TLD docs to v4_latest.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@43347 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/metadata/context/ResourceRootPath.java
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/metadata/context/ResourceRootPath.java
@@ -23,6 +23,5 @@ import java.io.Serializable;
 public class ResourceRootPath implements Serializable 
 {
     public static final String FOR_WIKI = "http://wiki.icefaces.org/display/ICE/";
-    public static final String FOR_ACE_TLD = "http://www.icefaces.org/docs/v3_latest/ace/tld/ace/";
-    public static final String FOR_ICE_TLD="http://www.icefaces.org/docs/v3_latest/compat/comps/tld/";
+    public static final String FOR_ACE_TLD = "http://www.icefaces.org/docs/v4_latest/ace/tld/ace/";
 }
