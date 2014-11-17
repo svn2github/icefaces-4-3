da495d424d4ba494d891e4b6317b100e506c036a
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-11-17 21:16:44 +0000

    ICE-10406 Add push storm test app.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@43535 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/core/test/push-test/src/main/java/org/icefaces/demo/push/Clock.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.demo.push;
+
+import org.icefaces.application.PortableRenderer;
+import org.icefaces.application.PushRenderer;
+import org.icefaces.bean.WindowDisposed;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@ManagedBean(name = "Clock")
+@ViewScoped
+@WindowDisposed
+public class Clock implements Serializable {
+    private SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss .SSS");
+    private Timer timer = new Timer();
+    private long interval = 1000;
+    private PortableRenderer renderer;
+    private TimerTask intervalNotifier;
+    private static int subCounter = 0;
+    private String id;
+
+
+    public Clock() {
+        intervalNotifier = new IntervalNotifier();
+        id = generateID();
+    }
+
+    @PostConstruct
+    public void init() {
+        renderer = PushRenderer.getPortableRenderer();
+        PushRenderer.addCurrentView(id);
+        timer.scheduleAtFixedRate(intervalNotifier, 0, interval);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        intervalNotifier.cancel();
+        timer.purge();
+        timer.cancel();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTime() {
+        return formatter.format(new Date());
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+        intervalNotifier.cancel();
+        timer.purge();
+        intervalNotifier = new IntervalNotifier();
+        timer.scheduleAtFixedRate(intervalNotifier, 0, interval);
+    }
+
+    private class IntervalNotifier extends TimerTask {
+        public void run() {
+            renderer.render(id);
+        }
+    }
+
+    private synchronized String generateID() {
+        return Integer.toString((++subCounter) + (hashCode() / 10000), 36).toUpperCase();
+    }
+}
