294ec4088f36e5aac4262cac9ed1c8fa9d7fa646
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-03-30 15:44:02 +0000

    Added auction demo to ace/ (currently just a mirror of core), with the intent of updating the demo to use ace components, ICEpush, and other new features
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@44325 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/ace/auction/src/main/java/org/icefaces/demo/auction/services/Comparator/ItemNameComparator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.demo.auction.services.Comparator;
+
+import org.icefaces.demo.auction.services.beans.AuctionItem;
+
+/**
+ * Sorts an AuctionItem using the field "name" in the order specified by the
+ * constructor's ascending attribute.
+ *
+ * @author ICEsoft Technologies Inc.
+ * @since 2.0
+ */
+public class ItemNameComparator extends AbstractItemComparator {
+
+    public ItemNameComparator(boolean ascending) {
+        super(ascending);
+    }
+
+    /**
+     * Compares its two arguments for order.  Returns a negative integer,
+     * zero, or a positive integer as the first argument is less than, equal
+     * to, or greater than the second.<p>
+     * <p/>
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer as the
+     *         first argument is less than, equal to, or greater than the
+     *         second.
+     */
+    @Override
+    public int compare(AuctionItem o1, AuctionItem o2) {
+        if (isAscending) {
+            return o1.getName().compareTo(o2.getName());
+        } else {
+            return o2.getName().compareTo(o1.getName());
+        }
+    }
+}
