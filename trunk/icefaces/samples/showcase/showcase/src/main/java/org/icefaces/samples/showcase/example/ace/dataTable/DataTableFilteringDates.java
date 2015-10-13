6624a540ec1b4e3daee39917173b1dc6d16f1f73
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-10-13 17:11:52 +0000

    ICE-10811 added 'Filtering Dates' and 'Filtering Ranges' showcase demos
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@46080 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/dataTable/DataTableFilteringDates.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.ace.dataTable;
+
+import java.io.Serializable;
+import java.util.*;
+
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+@ManagedBean(name= DataTableFilteringDates.BEAN_NAME)
+@CustomScoped(value = "#{window}")
+public class DataTableFilteringDates implements Serializable {
+    public static final String BEAN_NAME = "dataTableFilteringDates";
+	public String getBeanName() { return BEAN_NAME; }
+
+	private List<Event> events;
+	private String[] eventTypes = {"Wedding", "Birthday", "Anniversary", "Reunion", "Other"};
+	private List<SelectItem> eventOptions = new ArrayList<SelectItem>() {{
+		add(new SelectItem(""));
+		add(new SelectItem("Wedding"));
+		add(new SelectItem("Birthday"));
+		add(new SelectItem("Anniversary"));
+		add(new SelectItem("Reunion"));
+	}};
+
+    public DataTableFilteringDates() {
+		events = new ArrayList<Event>(100);
+		Random random = new Random();
+		for (int i = 0; i < 100; i++) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.setLenient(true);
+			calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + i);
+			Date date = calendar.getTime();
+			String event = eventTypes[random.nextInt(4)];
+			events.add(new Event(i, date, event));
+		}
+	}
+
+	public List<Event> getEvents() { return events; }
+	public void setEvents(List<Event> events) { this.events = events; }
+
+    public List<SelectItem> getEventOptions() { return eventOptions; }
+    public void setEventOptions(List<SelectItem> eventOptions) { this.eventOptions = eventOptions; }
+}
---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/dataTable/DataTableFilteringRanges.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.ace.dataTable;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+import org.icefaces.samples.showcase.dataGenerators.utilityClasses.DataTableData;
+
+@ManagedBean(name= DataTableFilteringRanges.BEAN_NAME)
+@CustomScoped(value = "#{window}")
+public class DataTableFilteringRanges implements Serializable {
+    public static final String BEAN_NAME = "dataTableFilteringRanges";
+	public String getBeanName() { return BEAN_NAME; }
+    private List<Car> carsData;
+    private List<SelectItem> accelOptions = new ArrayList<SelectItem>() {{
+        add(new SelectItem(""));
+        add(new SelectItem(5));
+        add(new SelectItem(10));
+        add(new SelectItem(15));
+    }};
+
+    /////////////---- CONSTRUCTOR BEGIN
+    public DataTableFilteringRanges() {
+        carsData = new ArrayList<Car>(DataTableData.getDefaultData());
+    }
+
+    /////////////---- GETTERS & SETTERS BEGIN
+    public List<Car> getCarsData() { return carsData; }
+    public void setCarsData(List<Car> carsData) { this.carsData = carsData; }
+
+    public List<SelectItem> getAccelOptions() {
+        return accelOptions;
+    }
+
+    public void setAccelOptions(List<SelectItem> accelOptions) {
+        this.accelOptions = accelOptions;
+    }
+}
