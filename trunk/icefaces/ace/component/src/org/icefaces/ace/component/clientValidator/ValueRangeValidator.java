ecb48a3aedb4095d1467dd950e709b84df0b3921
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-10-29 17:55:49 +0000

    ICE-10824 Implemented ace:clientValidateValueRange client-side validator.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@46151 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/component/clientValidator/ValueRangeValidator.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.ace.component.clientValidator;
+
+import org.icefaces.ace.component.message.Message;
+import org.icefaces.ace.renderkit.CoreRenderer;
+import org.icefaces.ace.util.ComponentUtils;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ValueRangeValidator extends ValueRangeValidatorBase {
+
+    public void encodeBegin(FacesContext context) throws IOException {
+        UIComponent validatedComponent = getParent();
+        if (validatedComponent instanceof Validateable) {
+            final Float min = getMin();
+            final Float max = getMax();
+            final String validatorConfiguration;
+            final String rule;
+            if (min == null) {
+                if (max == null) {
+                    return;
+                } else {
+                    rule = "max";
+                    validatorConfiguration = String.valueOf(max);
+                }
+            } else {
+                if (max == null) {
+                    rule = "min";
+                    validatorConfiguration = String.valueOf(min);
+                } else {
+                    rule = "range";
+                    validatorConfiguration = "[" + String.valueOf(min) + ", " + String.valueOf(max) + "]";
+                }
+            }
+
+            final Validateable v = (Validateable) validatedComponent;
+            final String id = v.getValidatedElementId();
+            final String messageClientId = (String) validatedComponent.getAttributes().get(Message.class.getName());
+            final UIComponent form = ComponentUtils.findParentForm(context, validatedComponent);
+            final List<UIComponent> children = form.getChildren();
+            final ResourceBundle bundle = CoreRenderer.getComponentResourceBundle(FacesContext.getCurrentInstance(), "org.icefaces.ace.resources.messages");
+            final String message = CoreRenderer.getLocalisedMessageFromBundle(bundle,
+                    "org.icefaces.ace.component.clientvalidation.", rule, "Required value is minimum {0} and maximum {1}.");
+
+            final StringBuffer script = new StringBuffer();
+            script.append("ice.ace.setupClientValidation('");
+            script.append(id);
+            script.append("', '");
+            script.append(rule);
+            script.append("', ");
+            script.append(validatorConfiguration);
+            script.append(", '");
+            script.append(messageClientId);
+            script.append("', '");
+            script.append(MessageFormat.format(message, min, max));
+            script.append("')");
+
+            children.add(new ScriptOutputWriter(script.toString()));
+        }
+    }
+
+    public void encodeEnd(FacesContext context) throws IOException {
+    }
+}
---
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/component/clientValidator/ValueRangeValidatorMeta.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.ace.component.clientValidator;
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+import org.icefaces.ace.resources.ACEResourceNames;
+import org.icefaces.resources.ICEResourceDependencies;
+import org.icefaces.resources.ICEResourceDependency;
+import org.icefaces.resources.ICEResourceLibrary;
+
+@Component(
+        tagName         = "clientValidateValueRange",
+        componentClass  = "org.icefaces.ace.component.clientValidator.ValueRangeValidator",
+        rendererClass   = "org.icefaces.ace.component.clientValidator.ValidatorRenderer",
+        generatedClass  = "org.icefaces.ace.component.clientValidator.ValueRangeValidatorBase",
+        extendsClass    = "javax.faces.component.UIComponentBase",
+        componentType   = "org.icefaces.ace.component.ValueRangeValidator",
+        rendererType    = "org.icefaces.ace.component.ValidatorRenderer",
+        componentFamily = "org.icefaces.ace.component",
+        tlddoc = "The ClientValidator is a ..... "
+)
+
+@ICEResourceLibrary(ACEResourceNames.ACE_LIBRARY)
+@ICEResourceDependencies({
+        @ICEResourceDependency(name = "jquery/validate/jquery.validate.js"),
+        @ICEResourceDependency(name = "jquery/validate/additional-methods.js"),
+        @ICEResourceDependency(name = "clientvalidator/clientvalidator.js"),
+})
+public class ValueRangeValidatorMeta extends UIComponentBaseMeta {
+        @Property(tlddoc = "Required minimum value.")
+        private Float min;
+
+        @Property(tlddoc = "Required maximum value.")
+        private Float max;
+}
