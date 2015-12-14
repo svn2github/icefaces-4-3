cf648790d46c595ee0ddd16101d1549969f52f42
carmen.cristurean <carmen.cristurean@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-12-14 22:18:26 +0000

    ICE-10874 modify ace:tooltip Delegate demo to enable tooltips display only for even-numbered rows via delegateDisplayListener, however this is not working; additional code may be required to be written for the tooltip component to enable this feature.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@46530 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/tooltip/DelegateTooltipBean.java
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/tooltip/DelegateTooltipBean.java
@@ -17,9 +17,14 @@
 package org.icefaces.samples.showcase.example.ace.tooltip; 
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
+
+import org.icefaces.samples.showcase.dataGenerators.utilityClasses.DataTableData;
+import org.icefaces.samples.showcase.example.ace.dataTable.Car;
 
 @ManagedBean(name= DelegateTooltipBean.BEAN_NAME)
 @CustomScoped(value = "#{window}")
@@ -29,6 +34,11 @@ public class DelegateTooltipBean implements Serializable {
 	public String getBeanName() { return BEAN_NAME; }
 	
 	private Object data;
+	private List<Car> carList = new ArrayList<Car>();
+	
+	public DelegateTooltipBean() {
+		carList = new ArrayList<Car>(DataTableData.getDefaultData());
+	}
 	
 	public Object getData() {
 		return data;
@@ -37,4 +47,45 @@ public class DelegateTooltipBean implements Serializable {
 	public void setData(Object data) {
 		this.data = data;
 	}
+			
+	public List<Car> getCarList() {
+		return carList;
+	}
+	public void setCarList(List<Car> carList) {
+		this.carList = carList;
+	}
+
+	private boolean cancelListener = false;
+
+    public boolean isCancelListener() {
+        return cancelListener;
+    }
+
+    public void setCancelListener(boolean cancelListener) {
+        this.cancelListener = cancelListener;
+    }
+        
+    public void listener(org.icefaces.ace.event.TooltipDelegateDisplayEvent event) {
+    	int index = 0;
+    	
+        for (int i=0; i<carList.size(); i++){
+        	
+        	index = ((Car)carList.get(i)).getId();
+        	
+        	if(cancelListener != false) {        	
+        		if (index%2==0){
+        			//odd rows
+        			System.out.println("index=" + index);
+        			System.out.println("cancelListener=" + cancelListener);
+        		
+        			event.cancelDisplay();                
+        		}
+	        	else {
+	        		//even rows
+	        		//do nothing      		
+	        	}
+        	}
+        }        
+    }
+    
 }
