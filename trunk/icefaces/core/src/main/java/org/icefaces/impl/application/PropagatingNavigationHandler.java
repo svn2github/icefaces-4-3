b909d8b6d30432e8046f137e193ed82f48e5d858
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2016-02-03 21:23:01 +0000

    ICE-10903 Acquire navigation case before navigation is executed to ensure that a non-null value is returned.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@47070 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/core/src/main/java/org/icefaces/impl/application/PropagatingNavigationHandler.java
+++ trunk/icefaces/core/src/main/java/org/icefaces/impl/application/PropagatingNavigationHandler.java
@@ -70,12 +70,12 @@ public class PropagatingNavigationHandler extends ConfigurableNavigationHandler 
             }
         }
         Object oldDOM = viewMap.get(DOMResponseWriter.OLD_DOM);
+        NavigationCase navigationCase = getNavigationCase(context, fromAction, outcome);
 
         wrapped.handleNavigation(context, fromAction, outcome);
 
         viewRoot = context.getViewRoot();
-        NavigationCase navigationCase = getNavigationCase(context, fromAction, outcome);
-        if (EnvUtils.isPortal() || (navigationCase != null && !navigationCase.isRedirect())) {
+        if (navigationCase != null && !navigationCase.isRedirect()) {
             viewMap = viewRoot.getViewMap();
             viewMap.putAll(propagated);
             if (null != oldDOM) {
