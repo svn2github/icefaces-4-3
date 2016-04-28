588f053f34f8f02dd5391d210038c984b31bfe34
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2016-04-28 21:26:36 +0000

    ICE-11014 improvement to replace the entire input element when setting it to type="password" and vice versa, instead of simply changing the type attribute on the same element object
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@48672 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/ace/component/resources/icefaces.ace/textentry/textentry.js
+++ trunk/icefaces/ace/component/resources/icefaces.ace/textentry/textentry.js
@@ -15,22 +15,23 @@
  */
 
 ice.ace.TextEntry = function(id, cfg) {
-    var jQ = ice.ace.jq;
     var inputId = id + "_input";
     var labelName = id + "_label";
     this.id = id;
     this.cfg = cfg;
     this.jqId = ice.ace.escapeClientId(id) + " input.ui-textentry";
-    this.jq = jQ(this.jqId);
-	var self = this;
+    this.jq = ice.ace.jq(this.jqId);
     var maxlength = cfg.maxlength;
 
     if (cfg.embeddedLabel) { // execute this when component is lazy loaded
 		if (this.jq.attr("name") == labelName) {
-			try { if (this.cfg.secret) this.jq.attr({type: 'password'}); } catch (e) {}
 			this.jq.attr({name: inputId});
 			this.jq.val("");
 			this.jq.removeClass("ui-input-label-infield");
+			if (this.cfg.secret) {
+				this.changeType('password');
+				this.jq.focus();
+			}
 		}
 	}
 
@@ -38,61 +39,17 @@ ice.ace.TextEntry = function(id, cfg) {
 		ice.ace.setResetValue(this.id, this.jq.val());
 
     if (cfg.autoTab) {
-        this.jq.keypress(
-            function(e) {
-                var curLength = this.value.length + 1, maxLength = this.maxLength;
-                var nextTabElement = ice.ace.TextEntry.nextTabElement(this);
-
-                if (curLength < maxLength || !nextTabElement) {
-                    return;
-                }
-                if (e.which < 32 || e.charCode == 0 || e.ctrlKey || e.altKey) {
-                    return;
-                }
-                e.preventDefault();
-                if (curLength == maxLength) {
-                    this.value += String.fromCharCode(e.which);
-                }
-
-				ice.setFocus();
-                nextTabElement.focus();
-
-				if (ice.ace.jq.browser.msie || ice.ace.jq.browser.webkit || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-					if (self.cfg.behaviors) {
-						if (self.cfg.behaviors.blur) {
-							ice.ace.ab(self.cfg.behaviors.blur);
-						}
-						if (self.cfg.behaviors.change) {
-							ice.ace.ab(self.cfg.behaviors.change);
-						}
-					}
-				}
-            }
-        );
+		this.attachAutoTabEvents();
     }
     if (cfg.embeddedLabel) {
-		var self = this;
-        this.jq.focus(
-            function() {
-                var input = jQ(this);
-                if (input.attr("name") == labelName) {
-					try { if (self.cfg.secret) input.attr({type: 'password'}); } catch (e) {}
-                    input.attr({name: inputId});
-                    input.val("");
-                    input.removeClass("ui-input-label-infield");
-                }
-            }).blur(
-            function() {
-                var input = jQ(this);
-                if (jQ.trim(input.val()) == "") {
-                    try { if (self.cfg.secret) input.attr({type: self.originalType}); } catch (e) {}
-                    input.attr({name: labelName});
-                    input.val(cfg.embeddedLabel);
-                    input.addClass("ui-input-label-infield");
-                }
-            });
+		this.attachEmbeddedLabelEvents();
     } else {
-		try { if (this.cfg.secret) this.jq.attr({type: 'password'}); } catch (e) {}
+		if (this.cfg.secret) {
+			this.changeType('password');
+			if (cfg.autoTab) this.attachAutoTabEvents();
+			var self = this;
+			setTimeout(function(){self.jq.focus();}, 0);
+		}
 	}
     this.jq.blur(function() {
         ice.setFocus();
@@ -100,44 +57,132 @@ ice.ace.TextEntry = function(id, cfg) {
     this.jq.change(function() {
         ice.setFocus();
     });
-    if (cfg.behaviors) {
-        ice.ace.jq.each(cfg.behaviors, function(name, behavior) {
-            if (name == 'charCount') {
-                var target = document.getElementById(inputId);
-                behavior.currLength = function() {
-                    return target.value.length;
-                };
-                behavior.charsRemaining = function() {
-                    return target.maxLength - target.value.length;
-                };
-                if (document.attachEvent) {
-                    //IE 7,8,9 handling -- backspace and delete keypresses do not trigger 'input' events
-                    self.jq.on('input', function (e) {
-                        if (e.target.value.length <= e.target.maxLength) {
-                            e.cancelBubble = true;
-                            ice.ace.ab(behavior);
-                        }
-                    });
-                    self.jq.on('keyup', function (e) {
-                        if (e.target.value.length <= e.target.maxLength) {
-                            ice.ace.ab(behavior);
-                        }
-                    });
-                } else {
-                    self.jq.on('input', function (e) {
-                        ice.ace.ab(behavior);
-                    });
-                }
-            } else {
-                self.jq.on(name, function () {
-                    ice.ace.ab(behavior);
-                });
-            }
-        });
-    }
+    if (cfg.behaviors) this.attachBehaviors();
     ice.onElementUpdate(inputId, function() {
         ice.ace.destroy(id);
     });
+};
+
+ice.ace.TextEntry.prototype.attachAutoTabEvents = function() {
+	var self = this;
+	this.jq.keypress(
+		function(e) {
+			var curLength = this.value.length + 1, maxLength = this.maxLength;
+			var nextTabElement = ice.ace.TextEntry.nextTabElement(this);
+
+			if (curLength < maxLength || !nextTabElement) {
+				return;
+			}
+			if (e.which < 32 || e.charCode == 0 || e.ctrlKey || e.altKey) {
+				return;
+			}
+			e.preventDefault();
+			if (curLength == maxLength) {
+				this.value += String.fromCharCode(e.which);
+			}
+
+			ice.setFocus();
+			nextTabElement.focus();
+
+			if (ice.ace.jq.browser.msie || ice.ace.jq.browser.webkit || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+				if (self.cfg.behaviors) {
+					if (self.cfg.behaviors.blur) {
+						ice.ace.ab(self.cfg.behaviors.blur);
+					}
+					if (self.cfg.behaviors.change) {
+						ice.ace.ab(self.cfg.behaviors.change);
+					}
+				}
+			}
+		}
+	);
+};
+
+ice.ace.TextEntry.prototype.attachEmbeddedLabelEvents = function() {
+	var self = this;
+    var inputId = this.id + "_input";
+    var labelName = this.id + "_label";
+	this.jq.focus(
+		function() {
+			var input = ice.ace.jq(this);
+			if (input.attr("name") == labelName) {
+				input.attr({name: inputId});
+				input.val("");
+				input.removeClass("ui-input-label-infield");
+				if (self.cfg.secret) {
+					self.changeType('password');
+					if (self.cfg.autoTab) self.attachAutoTabEvents();
+					if (self.cfg.embeddedLabel) self.attachEmbeddedLabelEvents();
+					if (self.cfg.behaviors) self.attachBehaviors();
+					self.jq.focus();
+				}
+			}
+		}).blur(
+		function() {
+			ice.setFocus('');
+			var input = ice.ace.jq(this);
+			if (ice.ace.jq.trim(input.val()) == ""
+				&& !(self.cfg.behaviors && self.cfg.behaviors.blur)) {
+				input.attr({name: labelName});
+				input.val(self.cfg.embeddedLabel);
+				input.addClass("ui-input-label-infield");
+				if (self.cfg.secret) {
+					self.changeType(self.originalType);
+					if (self.cfg.autoTab) self.attachAutoTabEvents();
+					if (self.cfg.embeddedLabel) self.attachEmbeddedLabelEvents();
+					if (self.cfg.behaviors) self.attachBehaviors();
+				}
+			}
+		});
+};
+
+ice.ace.TextEntry.prototype.attachBehaviors = function() {
+	var self = this;
+    var inputId = this.id + "_input";
+	ice.ace.jq.each(this.cfg.behaviors, function(name, behavior) {
+		if (name == 'charCount') {
+			var target = document.getElementById(inputId);
+			behavior.currLength = function() {
+				return target.value.length;
+			};
+			behavior.charsRemaining = function() {
+				return target.maxLength - target.value.length;
+			};
+			if (document.attachEvent) {
+				//IE 7,8,9 handling -- backspace and delete keypresses do not trigger 'input' events
+				self.jq.on('input', function (e) {
+					if (e.target.value.length <= e.target.maxLength) {
+						e.cancelBubble = true;
+						ice.ace.ab(behavior);
+					}
+				});
+				self.jq.on('keyup', function (e) {
+					if (e.target.value.length <= e.target.maxLength) {
+						ice.ace.ab(behavior);
+					}
+				});
+			} else {
+				self.jq.on('input', function (e) {
+					ice.ace.ab(behavior);
+				});
+			}
+		} else {
+			self.jq.on(name, function () {
+				ice.ace.ab(behavior);
+			});
+		}
+	});
+};
+
+ice.ace.TextEntry.prototype.changeType = function(type) {
+	var newInput = '<input';
+	ice.ace.jq.each(this.jq.get(0).attributes, function(i, attr) {
+		if (attr.name == 'type' || attr.name == 'value') return true;
+		newInput += ' ' + attr.name + '="' + attr.value + '"';
+	});
+	newInput += ' type="'+type+'" value="'+this.jq.val()+'" />';
+	this.jq.replaceWith(newInput);
+    this.jq = ice.ace.jq(this.jqId);
 };
 
 // Original code copied from http://stackoverflow.com/a/7329696
