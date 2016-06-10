d6da6e5abeebfdc8989138532c0fb644b7e48ee9
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2016-06-10 21:00:28 +0000

    ICE-11056 made the Tooltip component class serializable
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@48933 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/ace/component/src/org/icefaces/ace/component/tooltip/Tooltip.java
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/component/tooltip/Tooltip.java
@@ -45,7 +45,7 @@ import javax.faces.event.FacesEvent;
 import javax.faces.event.AbortProcessingException;
 import org.icefaces.ace.event.TooltipDelegateDisplayEvent;
 
-public class Tooltip extends TooltipBase{
+public class Tooltip extends TooltipBase implements java.io.Serializable {
 
 	private static final String OPTIMIZED_PACKAGE = "org.icefaces.ace.component.";
 
