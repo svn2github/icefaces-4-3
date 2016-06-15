56ed71b64a879e3b20e11532badf55639e4956a2
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2016-06-15 16:29:12 +0000

    ICE-11064 Test for the existence of image directory lazily, not at application startup time.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@48948 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/samples/ace/emporium/src/main/java/org/icefaces/demo/emporium/bid/model/AuctionImage.java
+++ trunk/icefaces/samples/ace/emporium/src/main/java/org/icefaces/demo/emporium/bid/model/AuctionImage.java
@@ -46,59 +46,58 @@ public class AuctionImage implements Serializable {
 	public static final String IMAGE_LIBRARY = "items"; // Folder that should be under web/resources/ that contains our item images
 	public static final String DEFAULT_NAME = "unknown";
 	public static final String EXTENSION = ".jpg";
-	
-	private static final String parentDir = generateParentDir();
+	private static final String IMAGE_DIR = "/resources/" + IMAGE_LIBRARY;
+
+	private String parentDir;
 	private String[] cachedImagesList; // List of images (including extension)
-	
-	private static String generateParentDir() {
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-		String imageDirectory = "/resources/" + IMAGE_LIBRARY;
-		URL toReturn = null;
-		try {
-			toReturn = externalContext.getResource(imageDirectory);
-		} catch (MalformedURLException e) {
-			//ignore
+
+    public boolean isValidParentDir() {
+		if (parentDir == null) {
+			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+			String imageDirectory = IMAGE_DIR;
+			URL toReturn;
+			try {
+				toReturn = externalContext.getResource(imageDirectory);
+			} catch (MalformedURLException e) {
+				toReturn = null;
+			}
+
+			if (toReturn == null) {
+				log.log(Level.SEVERE, "Desired item image directory " + IMAGE_DIR + " does not exist or isn't readable. This means all images will be " + DEFAULT_NAME + EXTENSION);
+			} else {
+				Set<String> imagePaths = externalContext.getResourcePaths(imageDirectory);
+				log.info("Item image directory " + toReturn.getPath() + " valid with " + imagePaths.size() + " files.");
+			}
+			parentDir = imageDirectory;
 		}
 
-		if (toReturn == null) {
-			log.log(Level.SEVERE, "Desired item image directory " + toReturn + " does not exist or isn't readable. This means all images will be " + DEFAULT_NAME + EXTENSION);
-		} else {
-			Set<String> imagePaths = externalContext.getResourcePaths(imageDirectory);
-			log.info("Item image directory " + toReturn.getPath() + " valid with " + imagePaths.size() + " files.");
-		}
-		
-		return imageDirectory;
-	}
-
-	public boolean isValidParentDir() {
-		return parentDir != null;
-	}
+        return parentDir != null;
+    }
 
 	public static String staticConvertNameToImageName(String name) {
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 		// First of all we won't assign any images until we know we have a valid parentDir (ideally resources/items/) to pull from
-		if (parentDir != null) {
-			// The image name format is all lowercase, spaces replaced with underscores
-			if (StringUtil.validString(name)) {
-				name = name.toLowerCase();
-				if (name.contains(" ")) {
-					name = name.replaceAll(" ", "_");
-				}
-				
-				String imageName = new String(name);
-				
-				// Next we have to check that the image actually exists
-				URL toCheck = null;
-				try {
-					toCheck = externalContext.getResource(parentDir + "/" + imageName + EXTENSION);
-				} catch (MalformedURLException e) {
-					//ignore
-				}
-				if (toCheck != null) {
-					return imageName;
-				}
+		// The image name format is all lowercase, spaces replaced with underscores
+		if (StringUtil.validString(name)) {
+			name = name.toLowerCase();
+			if (name.contains(" ")) {
+				name = name.replaceAll(" ", "_");
+			}
+
+			String imageName = new String(name);
+
+			// Next we have to check that the image actually exists
+			URL toCheck = null;
+			try {
+				toCheck = externalContext.getResource(IMAGE_DIR + "/" + imageName + EXTENSION);
+			} catch (MalformedURLException e) {
+				//ignore
+			}
+			if (toCheck != null) {
+				return imageName;
 			}
 		}
+
 		return DEFAULT_NAME;
 	}
 
@@ -107,16 +106,18 @@ public class AuctionImage implements Serializable {
 	}
 
 	public String[] getImagesList() {
-		if (cachedImagesList == null & isValidParentDir()) {
+		if (cachedImagesList == null) {
 			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-			Set<String> fileNames = externalContext.getResourcePaths(parentDir);
-			TreeSet<String> imageFileNames = new TreeSet<String>();
-			for (String name : fileNames) {
-				if (name.toLowerCase().endsWith(EXTENSION) && (!name.toLowerCase().startsWith(DEFAULT_NAME))) {
-					imageFileNames.add(name);
+			Set<String> fileNames = externalContext.getResourcePaths(IMAGE_DIR);
+			if (isValidParentDir() && fileNames != null) {
+				TreeSet<String> imageFileNames = new TreeSet<String>();
+				for (String name : fileNames) {
+					if (name.toLowerCase().endsWith(EXTENSION) && (!name.toLowerCase().startsWith(DEFAULT_NAME))) {
+						imageFileNames.add(name);
+					}
 				}
+				cachedImagesList = imageFileNames.toArray(new String[0]);
 			}
-			cachedImagesList = imageFileNames.toArray(new String[0]);
 		}
 
 		return cachedImagesList;
