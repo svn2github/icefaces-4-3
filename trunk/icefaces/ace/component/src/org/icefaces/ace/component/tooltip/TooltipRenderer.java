4a4f9df749b3688a1688fd0f039bb65fc9406852
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2016-08-29 20:34:19 +0000

    ICE-11116 Factor out JS inline code into library function.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@49125 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/ace/component/src/org/icefaces/ace/component/tooltip/TooltipRenderer.java
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/component/tooltip/TooltipRenderer.java
@@ -143,20 +143,27 @@ public class TooltipRenderer extends CoreRenderer {
 		writer.startElement("script", null);
 		writer.writeAttribute("type", "text/javascript", null);
 
-        writer.write("ice.ace.jq(function() {");
-
         String delegateId = tooltip.getForDelegate();
         JSONBuilder jb = JSONBuilder.create();
-		jb.beginFunction("ice.ace.create")
-          .item("Tooltip")
-          .beginArray()
-          .item(clientId)
+		jb.beginFunction("ice.ace.Tooltip.activate")
 	      .beginMap()
           .entry("global", global)
           .entry("id", clientId)
           .entry("displayListener", (tooltip.getDisplayListener() != null || tooltip.getDelegateDisplayListener() != null));
 
 		if (tooltip.isSpeechBubble()) jb.entry("speechBubble", true);
+		//Position
+		jb.beginMap("position");
+		jb.entry("at", mapPosition(tooltip.getTargetPosition()))
+				.entry("my", mapPosition(tooltip.getPosition()))
+				.endMap();
+
+		encodeClientBehaviors(facesContext, tooltip, jb);
+		jb.entry("ariaEnabled", EnvUtils.isAriaEnabled(facesContext));
+		String style = tooltip.getStyle();
+		if (style != null) jb.entry("inlineStyle", style);
+		String styleClass = tooltip.getStyleClass();
+		if (styleClass != null) jb.entry("styleClass",styleClass);
 
 		if(!global && delegateId == null) {
 			owner = getTarget(facesContext, tooltip);
@@ -171,17 +178,12 @@ public class TooltipRenderer extends CoreRenderer {
 			} else {
 				jb.entry("forComponent", (String) owner);
 			}
-			writer.write(jb.toString());
-			writer.write(",content:{text:");
+			jb.endMap();
 			if(tooltip.getValue() == null)
-				writer.write("function() {return document.getElementById('" + clientId + "_content').innerHTML}");
+				jb.item("");
 			else {
-				writer.write("'");
-				writer.write(ComponentUtils.getStringValueToRender(facesContext, tooltip).replaceAll("'", "\\\\'"));
-				writer.write("'");
+				jb.item(ComponentUtils.getStringValueToRender(facesContext, tooltip).replaceAll("'", "\\\\'"));
 			}
-
-			writer.write("},");
 		} else if (delegateId != null) {
 			UIComponent delegateComponent = CoreComponentUtils.findComponentInView(facesContext.getViewRoot(), delegateId);
 			if (delegateComponent != null && delegateComponent instanceof Delegate) {
@@ -190,56 +192,31 @@ public class TooltipRenderer extends CoreRenderer {
 			} else {
 				throw new FacesException("Cannot find delegate component \"" + delegateId + "\" in view or it is not an instance of <ace:delegate>.");
 			}
-			writer.write(jb.toString());
-			writer.write(",content:{text:");
+			jb.endMap();
 			if(tooltip.getValue() == null)
-				writer.write("function() {return document.getElementById('" + clientId + "_content').innerHTML}");
+				jb.item("");
 			else {
-				writer.write("'");
-				writer.write(ComponentUtils.getStringValueToRender(facesContext, tooltip).replaceAll("'", "\\\\'"));
-				writer.write("'");
+				jb.item(ComponentUtils.getStringValueToRender(facesContext, tooltip).replaceAll("'", "\\\\'"));
 			}
-
-			writer.write("},");
-		} else {
-			writer.write(jb.toString());
-			writer.write(",");
 		}
 
-		jb = JSONBuilder.create();
 		//Events
-		jb.beginMap("show")
-			.entry("event", tooltip.getShowEvent())
-			.entry("delay", tooltip.getShowDelay())
-			.entry("effect", createShowEffectFunction(tooltip.getShowEffect(), tooltip.getShowEffectLength()), true)
-		.endMap();
+		jb.beginArray()
+				.item(tooltip.getShowEvent())
+				.item(tooltip.getShowDelay())
+				.item(createShowEffectFunction(tooltip.getShowEffect()))
+				.item(tooltip.getShowEffectLength())
+				.endArray();
 
-		jb.beginMap("hide")
-			.entry("event", tooltip.getHideEvent())
-			.entry("delay", tooltip.getHideDelay())
-			.entry("fixed", true)
-			.entry("effect", createHideEffectFunction(tooltip.getHideEffect(), tooltip.getHideEffectLength()), true)
-		.endMap();
+		jb.beginArray()
+				.item(tooltip.getHideEvent())
+				.item(tooltip.getHideDelay())
+				.item(createHideEffectFunction(tooltip.getHideEffect()))
+				.item(tooltip.getHideEffectLength())
+				.endArray();
 
-		//Position
-		jb.beginMap("position");
-        String container = owner == null || owner instanceof ArrayList ? "document.body" : "ice.ace.jq(ice.ace.escapeClientId('" + owner +"')).parent()";
-        //jb.entry("container", container, true)
-		jb.entry("at", mapPosition(tooltip.getTargetPosition()))
-		.entry("my", mapPosition(tooltip.getPosition()))
-		.endMap();
-
-        encodeClientBehaviors(facesContext, tooltip, jb);
-        jb.entry("ariaEnabled", EnvUtils.isAriaEnabled(facesContext));
-		String style = tooltip.getStyle();
-		if (style != null) jb.entry("inlineStyle", style);
-		String styleClass = tooltip.getStyleClass();
-		if (styleClass != null) jb.entry("styleClass",styleClass);
-//        jb.entry("ariaEnabled", false);
-		jb.endMap().endArray().endFunction();
+		jb.endFunction();
 		writer.write(jb.toString());
-
-		writer.write("});");
 
         writer.endElement("script");
         writer.endElement("span");
@@ -304,24 +281,24 @@ public class TooltipRenderer extends CoreRenderer {
 		}
 	}
 	
-	private String createShowEffectFunction(String effect, int length) {
+	private String createShowEffectFunction(String effect) {
 		if ("fade".equalsIgnoreCase(effect)) 
-			return "function(){ice.ace.jq(this).fadeIn(" + length + ");}";
+			return "fadeIn";
 		if ("slide".equalsIgnoreCase(effect))
-			return "function(){ice.ace.jq(this).slideDown(" + length + ");}";
+			return "slideDown";
 		if ("grow".equalsIgnoreCase(effect))
-			return "function(){ice.ace.jq(this).show(" + length + ");}";
-		return "function(){ice.ace.jq(this).show();}";
+			return "show";
+		return "show";
 	}
 	
-	private String createHideEffectFunction(String effect, int length) {
+	private String createHideEffectFunction(String effect) {
 		if ("fade".equalsIgnoreCase(effect)) 
-			return "function(){ice.ace.jq(this).fadeOut(" + length + ");}";
+			return "fadeOut";
 		if ("slide".equalsIgnoreCase(effect))
-			return "function(){ice.ace.jq(this).slideUp(" + length + ");}";
+			return "slideUp";
 		if ("grow".equalsIgnoreCase(effect))
-			return "function(){ice.ace.jq(this).hide(" + length + ");}";
-		return "function(){ice.ace.jq(this).hide();}";
+			return "hide";
+		return "hide";
 	}
 		
 	private ArrayList<String> collectClientIds(FacesContext context, UIComponent container, String id) {
