189d3459a2945df06c32d5fe310f75455fbe2926
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2017-02-03 22:05:08 +0000

    ICE-11230
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@50375 8668f098-c06c-11db-ba21-f49e70c34f74

--- trunk/icefaces/ace/component/src/org/icefaces/ace/renderkit/CoreRenderer.java
+++ trunk/icefaces/ace/component/src/org/icefaces/ace/renderkit/CoreRenderer.java
@@ -56,52 +56,6 @@ import org.icefaces.ace.util.JSONBuilder;
 public class CoreRenderer extends Renderer {
     private static final Logger logger = Logger.getLogger(CoreRenderer.class.getName());
 
-    /**
-     * Not all text for components are easy to make attributes for components,
-     * especially those for accessibility.  Default values are made available
-     * within the ace jar messages properties files.  Users may override the
-     * key if they prefer their own, or their locale is not available.
-     * @return reference to resource bundle to get localised text for rendering
-     */
- /*   public static ResourceBundle getComponentResourceBundle(FacesContext context, String ACE_MESSAGES_BUNDLE){
-        Locale locale = context.getViewRoot().getLocale();
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        String bundleName = context.getApplication().getMessageBundle();
-        if (classLoader == null) {
-            classLoader = bundleName.getClass().getClassLoader();
-        }
-        if (bundleName == null) {
-            bundleName = ACE_MESSAGES_BUNDLE;
-        }
-        return ResourceBundle.getBundle(bundleName, locale, classLoader);
-    }  */
-
-    /**
-     *
-     * @param bundle
-     * @param MESSAGE_KEY_PREFIX
-     * @param key
-     * @param defaultValue
-     * @return localized or internationalized String value from message bundle
-     */
- /*   public static String getLocalisedMessageFromBundle(ResourceBundle bundle,
-                                                        String MESSAGE_KEY_PREFIX,
-                                                        String key,
-                                                        String defaultValue){
-         if (null == bundle) {
-             return defaultValue;
-         }
-         String label=defaultValue;
-         try {
-             label = bundle.getString(MESSAGE_KEY_PREFIX + key);
-         } catch(MissingResourceException mre){
-             if (logger.isLoggable(Level.FINE)) {
-                 logger.fine(" BUNDLE missing property : "+key+" defaultValue used : "+defaultValue);
-             }
-         }
-         return label;
-    }  */
-	
 	protected void renderChildren(FacesContext facesContext, UIComponent component) throws IOException {
 		for (Iterator<UIComponent> iterator = component.getChildren().iterator(); iterator.hasNext();) {
 			UIComponent child = (UIComponent) iterator.next();
