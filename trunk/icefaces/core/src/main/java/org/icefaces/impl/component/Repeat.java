d0c433c2f70071fee98b41a2b50d4d9f8ad0ee6f
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2017-02-06 23:37:08 +0000

    ICE-10997 Add icecore:repeat implementation.
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@50378 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/core/src/main/java/org/icefaces/impl/component/Repeat.java
@@ -0,0 +1,71 @@
+package org.icefaces.impl.component;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIData;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import java.io.IOException;
+import java.util.Map;
+
+public class Repeat extends UIData {
+
+    /**
+     * <p>Return the request-scope attribute under which the iteration status for the
+     * current row will be exposed.  This property is
+     * <strong>not</strong> enabled for value binding expressions.</p>
+     */
+    public String getVarStatus() {
+        return (String) getStateHelper().get("varStatus");
+    }
+
+
+    /**
+     * <p>Set the request-scope attribute under which the iteration status for the
+     * current row wil be exposed.</p>
+     *
+     * @param var The new request-scope attribute name
+     */
+    public void setVarStatus(String var) {
+        getStateHelper().put("varStatus", var);
+    }
+
+    public void encodeChildren(FacesContext context) throws IOException {
+        String var = getVar();
+        String varStatus = getVarStatus();
+        Map<String, Object> requestMap = context.getExternalContext().getRequestMap();
+        DataModel model = getDataModel();
+        int first = getFirst();
+        int rows = getRows();
+        int index = first < rows ? first : 0;
+        rows = rows == 0 ? getDataModel().getRowCount() : rows;
+
+        while (model.isRowAvailable() && index < first + rows) {
+            setRowIndex(index);
+            Object item = model.getRowData();
+            requestMap.put(var, item);
+            if (varStatus != null) {
+                requestMap.put(varStatus, new VarStatus(first, first + rows, index));
+            }
+            super.encodeChildren(context);
+            resetClientIDs(this);
+            index++;
+        }
+        requestMap.remove(var);
+        requestMap.remove(varStatus);
+    }
+
+    private void resetClientIDs(UIComponent component) {
+        for (UIComponent child: component.getChildren()) {
+            child.setId(child.getId());
+            resetClientIDs(child);
+        }
+    }
+
+    public boolean getRendersChildren() {
+        return true;
+    }
+
+    public String getRendererType() {
+        return null;
+    }
+}
