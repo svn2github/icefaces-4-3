2d6e1ba2e28fabb54a33580a2b26590c2dceac4a
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2017-06-06 21:47:57 +0000

    ICE-11286 added overview demo to showcase
    git-svn-id: http://anonsvn.icesoft.org/repo/icefaces4@51592 8668f098-c06c-11db-ba21-f49e70c34f74

---
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/drawerpanel/DrawerPanelBean.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.samples.showcase.example.ace.drawerpanel;
+
+import org.icefaces.samples.showcase.example.ace.schedule.RandomEventGenerator;
+import org.icefaces.ace.model.schedule.ScheduleEvent;
+import org.icefaces.ace.model.schedule.LazyScheduleEventList;
+
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.TimeZone;
+
+@ManagedBean(name= DrawerPanelBean.BEAN_NAME)
+@CustomScoped(value = "#{window}")
+public class DrawerPanelBean implements Serializable {
+    public static final String BEAN_NAME = "drawerPanelBean";
+	public String getBeanName() { return BEAN_NAME; }
+
+	public DrawerPanelBean() {
+		
+	}
+
+	private String position = "left";
+	public String getPosition() { return position; }
+	public void setPosition(String position) { this.position = position; }
+
+	private boolean showHandleOpen = true;
+	public boolean isShowHandleOpen() { return showHandleOpen; }
+	public void setShowHandleOpen(boolean showHandleOpen) { this.showHandleOpen = showHandleOpen; }
+
+	private boolean showHandleClose = true;
+	public boolean isShowHandleClose() { return showHandleClose; }
+	public void setShowHandleClose(boolean showHandleClose) { this.showHandleClose = showHandleClose; }
+
+	private boolean divContainer = false;
+	public boolean isDivContainer() { return divContainer; }
+	public void setDivContainer(boolean divContainer) { this.divContainer = divContainer; }
+
+	public String getContainer() {
+		if (divContainer) return "div";
+		else return "window";
+	}
+}
--- trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/view/menu/data/CentralDataList.java
+++ trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/view/menu/data/CentralDataList.java
@@ -534,6 +534,12 @@ public class CentralDataList {
 							 "accordionpanel", "accordionPanelDynamic.xhtml", "AccordionPanelDynamicBean.java"),
 					new Demo(msgs("menu.ace.accordionpanel.subMenu.effect"), msgs("example.ace.accordionpanel.effect.description"),
 							 "accordionpanel", "accordionPanelEffect.xhtml", "AccordionPanelEffectBean.java")),
+				new ComponentGroup("ace:drawerPanel", new DemoResource[] {
+						new DemoResource(DemoResource.Type.WIKI, "ace:drawerPanel", "DrawerPanel"),
+						new DemoResource(DemoResource.Type.TLD, "ace:drawerPanel", "drawerPanel.html")
+				},
+					new Demo(msgs("overview"), msgs("example.ace.drawerpanel.description"),
+							 "drawerpanel", "drawerPanel.xhtml", "DrawerPanelBean.java")),
 				new ComponentGroup("ace:panel", "Panel", "panel.html",
 						new Demo(msgs("overview"), msgs("example.ace.panel.description"),
 								 "panel", "panel.xhtml", "PanelBean.java"),
